a4e4004170060a73909289823d747e19e31b6743
x14119641 <daniel.gil.romero24@gmail.com>
2025-05-07 16:50:47 +0200

    Added manifest

---
+++ custom_addons/manresa_demo/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'MQTT MAnresa Demo',
+    'version': '1.0',
+    'summary': 'Live MQTT data display in Odoo',
+    'description': 'Fetches counter from mosquitto.',
+    'author': 'Daniel',
+    'category': 'Tools',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
